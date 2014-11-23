ecdbcdc9a2a4198ba10a14c87f8caf64b8f0d655
Cotes <cristian.cotes@urv.cat>
2014-11-23 02:30:15 +0100

    Overlay controller

---
+++ src/com/stacksync/desktop/gui/shell/OverlayController.java
@@ -0,0 +1,48 @@
+package com.stacksync.desktop.gui.shell;
+
+/**
+ *
+ * @author cotes
+ */
+public class OverlayController {
+    
+    public OverlayController() {}
+    
+    public void initialize() {
+        // connect with overlay library
+        
+        // Enable overlay
+        
+        // Register icons
+        
+        // Draw overlays
+    }
+    
+    public void stop() {
+        
+        // Disable overlays
+        
+        // Disconnect overlay library
+    }
+    
+    public void refreshFile() {
+        
+    }
+    
+    public void drawOverlay() {
+        
+    }
+    
+    public void removeOverlay() {
+        
+    }
+    
+    private void registerOverlay() {
+        
+    }
+    
+    private void unregisterOverlay() {
+        
+    }
+    
+}
